6b6bef3893884b5e3a37c7ed614151fdbe63d90c
Badi Labassi <blabassi@aldogroup.com>
2016-10-17 12:02:35 -0400

    Added more Helpers

--- lib/middleman-aldo/helpers.rb
+++ lib/middleman-aldo/helpers.rb
@@ -303,8 +303,57 @@ module Middleman
         multiplier * value.to_i
       end
 
+      def getItemByPosition(grid_position, page_data_request)
+        item_by_position = page_data_request.find { |k| k['grid_position'] == grid_position }
+        item_by_position ? item_by_position : '<span>no item found!</span>'
+      end
 
+      def getItemByPositionAndType(grid_position, page_type, page_data_request)
+        item_by_position = page_data_request.find {|k| k['grid_position'] == grid_position && k['type'] == page_type }
+        item_by_position ? item_by_position : '<span>no item found!</span>'
+      end
 
+      def getItemByType(type, page_data_request)
+        item_by_position = page_data_request.find { |k| k['type'] == type}
+        item_by_position ? item_by_position : '<span>no item found!</span>'
+      end
+
+      def getItemByPage(page, page_data_request)
+        item_by_position = page_data_request.find { |k| k['page'] == page}
+        item_by_position ? item_by_position : '<span>no item found!</span>'
+      end
+
+      def getAllItemsByPosition(grid_position, page_data_request)
+        item_by_position = page_data_request.find_all {|k| k['grid_position'] == grid_position }
+        item_by_position ? item_by_position : '<span>no item found!</span>'
+      end
+
+      def getAllItemsByType(type, page_data_request)
+        item_by_position = page_data_request.find_all {|k| k['type'] == type }
+        item_by_position ? item_by_position : '<span>no item found!</span>'
+      end
+
+      def getAllItemsByPage(page, page_data_request)
+        item_by_position = page_data_request.find_all { |k| k['page'] == page }
+        item_by_position ? item_by_position : '<span>no item found!</span>'
+      end
+
+      def getCell(grid_position, column_name, page_data_request)
+        getItemByPosition(grid_position, page_data_request)[column_name]
+      end
+
+      def getData(data_type, data_name, page_data)
+        request = page_data.find_all {|k| k["#{data_type}"].match /#{data_name}/}
+        if request.length == 1
+          return request[0]
+        else
+          return request ? request : 'Error: No Data Found'
+        end
+      end
+
+      def getAllData(data_type, data_name, page_data)
+        return page_data.find_all { |k| k["#{data_type}"].match /#{data_name}(.*)/ }
+      end
     end
   end
 end
