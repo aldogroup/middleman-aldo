6c88199d46e889c6d68f5ffdbd94561eee2e3593
Richard Man <rman@aldogroup.com>
2017-12-12 11:41:34 -0500

    fixing

--- lib/middleman-aldo/extension.rb
+++ lib/middleman-aldo/extension.rb
@@ -95,7 +95,7 @@ module Middleman
               next if proxy_lang == '.' or proxy_lang == '..'
               localeID = "#{proxy_lang}".split('.')[0]
               newLocaleID = "#{proxy_lang}".split('.')[0]
-              modLocaleID = newLocaleID.replace(/\_/g, '-')
+              # modLocaleID = newLocaleID.replace(/\_/g, '-')
 
               require 'pry'
               binding.pry
